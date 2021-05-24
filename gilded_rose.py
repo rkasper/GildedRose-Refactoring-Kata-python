4270e62e2d92d07626f5ac815b13878561ba6539
Richard Kasperowski <r@kasperowski.com>
2021-05-24 18:41:25 -0400

    Add type hints

--- gilded_rose.py
+++ gilded_rose.py
@@ -65,6 +65,10 @@ class GildedRose(object):
 
 
 class Item:
+    name: str
+    sell_in: int
+    quality: int
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
