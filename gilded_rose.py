f9f05bda55528649bbb65ae141b4b2429b911856
Richard Kasperowski <r@kasperowski.com>
2021-05-24 18:31:07 -0400

    Completed initial refactoring

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+MAX_QUALITY = 50
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+AGED_BRIE = "Aged Brie"
+
 
 class GildedRose(object):
 
@@ -6,34 +11,57 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
+        item: Item
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            self.handle_if_normal_item(item)
+            self.handle_if_sulfuras(item)
+            self.handle_if_aged_brie(item)
+            self.handle_if_backstage_passes(item)
+
+    @staticmethod
+    def handle_if_normal_item(item):
+        if not (item.name == AGED_BRIE or BACKSTAGE_PASSES == item.name or SULFURAS == item.name):
+            item.sell_in -= 1
+            if item.sell_in > 0:
+                item.quality -= 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.quality -= 2
+            if item.quality < 0:
+                item.quality = 0
+
+    @staticmethod
+    def handle_if_sulfuras(item):
+        if SULFURAS == item.name:
+            pass
+
+    @staticmethod
+    def handle_if_aged_brie(item):
+        if AGED_BRIE == item.name:
+            item.sell_in -= 1
+            if item.sell_in > 0:
+                item.quality += 1
+            else:
+                item.quality += 2
+            if item.quality > MAX_QUALITY:
+                item.quality = MAX_QUALITY
+
+    @staticmethod
+    def handle_if_backstage_passes(item):
+        # Backstage Passes
+        if BACKSTAGE_PASSES == item.name:
+            item.sell_in -= 1
+
+            if item.quality < MAX_QUALITY:
+                item.quality += 1
+                if item.sell_in < 11:
+                    if item.quality < MAX_QUALITY:
+                        item.quality += 1
+                if item.sell_in < 6:
+                    if item.quality < MAX_QUALITY:
+                        item.quality += 1
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                item.quality = 0
 
 
 class Item:
